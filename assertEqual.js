dceba84394dc32ab3f18a95cf1300879efb118ff
Anmol Tripathi <tripathianmol@protonmail.com>
2022-11-18 09:46:49 -0500

    assert equal code

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
