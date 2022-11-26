1fcae5299dc335610e126d58f841a50ab470473e
Anmol Tripathi <tripathianmol@protonmail.com>
2022-11-26 01:17:50 -0500

    assert added

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+const eqArrays = function (arr1, arr2) {
+    if (arr1.length != arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
