b4182daa087f399a07a70c970ef9a58d0b2b93c7
Anmol Tripathi <tripathianmol@protonmail.com>
2022-11-28 22:45:37 -0500

    findkeybyvalue

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, val) => {
+  let result = undefined;
+  
+  for (let key in Object.keys(obj)) {
+    if (obj[key] === val) {
+      result = key;
+      break;
+    }
+  }
+
+  return result;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
