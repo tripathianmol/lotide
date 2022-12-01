588a7e412ac01f8d6d9a9c869c389fa8ba12a09b
Anmol Tripathi <tripathianmol@protonmail.com>
2022-11-30 19:16:12 -0500

    all

---
+++ map.js
@@ -0,0 +1,36 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🟢🟢🟢 Assertion Passed`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed`);
+  }
+};
+
+
